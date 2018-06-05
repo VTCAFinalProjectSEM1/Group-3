854b2201cb4d89877a246bd0fe556ca0da871af8
HoangThiThuy2898 <thuyht.nde16034@vtc.edu.vn>
2018-06-05 17:50:18 +0700

    More

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Group_3
         static void Main(string[] args)
         {
             Console.WriteLine("Hoang Thi Thuy"); 
+            Console.WriteLine("Trinh Van Thuong");
+            
         }
     }
 }
