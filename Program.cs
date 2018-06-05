7c500bb2922d387bda556a084d3ac5267532f129
VTCATrinhVanThuong <thuongtv.nde17065@vtc.edu.vn>
2018-06-05 17:45:12 +0700

    change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Group_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hoang Thi Thuy");
         }
     }
 }
