4fdf41920fae7a5e61ca093123d10d55f3f3fe46
VTCNguyenQuangMinh <minhnq.nde17050@vtc.edu.vn>
2018-06-06 10:04:57 +0700

    test

--- Program.cs
+++ Program.cs
@@ -5,12 +5,9 @@ namespace Group_3
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Group!"); 
-            Console.WriteLine("Hoang Thi Thuy"); 
-            Console.WriteLine("Trinh Van Thuong");
-            Console.WriteLine("Nguyen Quang Minh"); 
+        { 
             Console.WriteLine("We are the team <3");
+
         }
     }
 }
